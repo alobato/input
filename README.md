3adcd9bdca80c3e29cf70f719ac1331a6231d0d6
Arley Lobato <arleylobato@gmail.com>
2019-08-08 00:59:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Input
+
+> Input react components
+
+## Install
+
+```sh
+yarn add @alobato/input
+```
+
+## Usage
+
+```js
+import Input from '@alobato/input'
+...
+
+<Input placeholder='Name' value={name} onChange={e => setName(e.target.value)} />
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components'
+import { width, height } from 'styled-system'
+
+export default styled.input`
+  -webkit-appearance: none;
+  color: inherit;
+  font-size: inherit;
+  font-family: inherit;
+  line-height: 1.2;
+  padding: 7px 10px;
+  width: 100%;
+  transition: all 0.3s;
+  ${width}
+  ${height}
+  border-radius: 3px;
+  border: 1px solid ${props => props.theme.colors.grey300};
+  background-color: ${props => props.theme.colors.white};
+  ::placeholder {
+    color: ${props => props.theme.colors.grey300};
+  }
+  ${props => props.withError && css`
+    border-color: ${props.theme.colors.error};
+  `}
+  &:focus {
+    outline: 0;
+    box-shadow: 0 0 0 3px ${props => props.theme.colors.primary200};
+    border: 1px solid ${props => props.theme.colors.primary400};
+  }
+	&:disabled {
+		background-color: ${props => props.theme.colors.grey100};
+		border: 1px solid ${props => props.theme.colors.grey200};
+		cursor: not-allowed;
+	}
+`
