e124cbb7826488b17497cebc94a98ebbf35d475c
Arley Lobato <arleylobato@gmail.com>
2019-08-16 12:21:42 +0100

    Themes optional

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ export default styled.input`
   ${width}
   ${height}
   border-radius: 3px;
-  border: 1px solid ${props => props.theme.colors.grey300};
-  background-color: ${props => props.theme.colors.white};
+  border: 1px solid ${props => (props.theme && props.theme.color && props.theme.colors.grey300) ? props.theme.colors.grey300 : 'hsla(203, 20%, 76%, 1)'};
+  background-color: ${props => (props.theme && props.theme.color && props.theme.colors.white) ? props.theme.colors.white : 'white'};
   ::placeholder {
-    color: ${props => props.theme.colors.grey300};
+    color: ${props => (props.theme && props.theme.color && props.theme.colors.grey300) ? props.theme.colors.grey300 : 'hsla(203, 20%, 76%, 1)'};
   }
   ${props =>
     props.withError &&
     css`
-      border-color: ${props.theme.colors.error};
+      border-color: ${props => (props.theme && props.theme.color && props.theme.colors.error) ? props.theme.colors.error : 'hsla(6, 66%, 47%, 1)'};
     `}
   &:focus {
     outline: 0;
-    box-shadow: 0 0 0 3px ${props => props.theme.colors.primary200};
-    border: 1px solid ${props => props.theme.colors.primary400};
+    box-shadow: 0 0 0 3px ${props => (props.theme && props.theme.color && props.theme.colors.grey200) ? props.theme.colors.grey200 : 'hsla(202, 20%, 86%, 1)'};
+    border: 1px solid ${props => (props.theme && props.theme.color && props.theme.colors.primary400) ? props.theme.colors.primary400 : 'hsla(205,  79%, 66%, 1)'};
   }
   &:disabled {
-    background-color: ${props => props.theme.colors.grey100};
-    border: 1px solid ${props => props.theme.colors.grey200};
+    background-color: ${props => (props.theme && props.theme.color && props.theme.colors.grey100) ? props.theme.colors.grey100 : 'hsla(201, 20%, 96%, 1)'};
+    border: 1px solid ${props => (props.theme && props.theme.color && props.theme.colors.grey200) ? props.theme.colors.grey200 : 'hsla(202, 20%, 86%, 1)'};
     cursor: not-allowed;
   }
 `
